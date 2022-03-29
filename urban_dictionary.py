1c0b8fc5f130d2a5b52910843509e29e57c65553
Li Xiang <lixiang@Little-Silver.hitronhub.home>
2022-03-29 03:12:21 -0600

    added python 3 version code for macOS 12 users

--- urban_dictionary.py
+++ urban_dictionary.py
@@ -5,7 +5,8 @@ import json
 
 url = "https://mashape-community-urban-dictionary.p.rapidapi.com/define"
 
-querystring = {"term":sys.stdin}
+# querystring = {"term":sys.stdin}
+querystring = {"term":"wood"}
 
 headers = {
     'x-rapidapi-key': "0e483cd0d4msh6797a8eb38297d2p1d26a2jsn0731d2d0c4d5",
@@ -16,16 +17,30 @@ response = requests.request("GET", url, headers=headers, params=querystring)
 
 res = json.loads(response.text)['list']
 
+# print(res)
+
 def get_definitions(res_list):
     definitions = []
     c = 1
-    for i in res_list:
+    for i in res_list: 
+        # Python 2 version
         d = i['definition'].replace('[', '').replace(']','').encode('utf-8')
         e = i['example'].replace('[', '').replace(']','').encode('utf-8')
         vote = str(str(i['thumbs_up'])+" ▲ | ▼ "+str(i['thumbs_down']).encode('utf-8'))
-        output = str(str(c)+'.\n\n'+d+'\n\n'+'Example:\n'+e+'\n\n\n'+vote).replace('\n','[<>]')
+        date = i['written_on'][:10].encode('utf-8')
+        output = str(str(c)+'.\n\n'+d+'\n\n'+'Example:\n'+e+'\n\n\n'+vote+'\n\n'+date).replace('\n','[<>]')
         definitions.append(output)
         c+=1
+
+        # Python 3 version
+        # d = i['definition'].replace('[', '').replace(']','')
+        # e = i['example'].replace('[', '').replace(']','')
+        # vote = str(str(i['thumbs_up'])+" ▲ | ▼ "+str(i['thumbs_down']))
+        # date = i['written_on'][:10]
+        # output = str(str(c)+'.\n\n'+d+'\n\n'+'Example:\n'+e+'\n\n\n'+vote+'\n\n'+date).replace('\n','[<>]')
+        # definitions.append(output)
+        # c+=1
+    
     return definitions
 
 res_list = get_definitions(res)
